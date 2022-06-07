1da13be9f5c4efb1a998cc6bba5af1754a545dbf
Yann Bohbot <yannbohbot@gmail.com>
2022-06-08 00:52:48 +0300

    Initial commit from Create Next App

---
+++ lib/revalidate.ts
@@ -0,0 +1,24 @@
+import { HttpMethod } from "@/types";
+
+export async function revalidate(domain?: string | null, slug?: string) {
+  const urlPaths = [`/${slug}`, `/`];
+
+  // refer to https://solutions-on-demand-isr.vercel.app/ for more info on bulk/batch revalidate
+  try {
+    await Promise.all(
+      urlPaths.map((urlPath) =>
+        fetch(`${domain}/api/revalidate`, {
+          method: HttpMethod.POST,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            urlPath,
+          }),
+        })
+      )
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
